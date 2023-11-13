2a2df9f327b200df35acea5172e953824bde4c9a
Nathan Parchment <54561224+Nathan-47@users.noreply.github.com>
2023-11-13 18:05:07 +0000

    Add files via upload

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -15,7 +15,7 @@ const Landing = () => {
             </p>
             <div className="mt-10 flex gap-x-6">
               <NavLink to="projects">
-              <div className="rounded-md bg-[#d3d3d3] px-3.5 py-2.5 text-#1d1d1d shadow-sm hover:bg-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              <div className="rounded-md bg-[#d3d3d3] px-3.5 py-2.5 text-#1d1d1d shadow-sm hover:bg-green-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-100"
               >
                 View Projects
                 </div>
