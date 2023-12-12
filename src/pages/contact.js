4b289d3ba6ab96e01773ffc046b9608997afb8a6
Nathan Parchment <54561224+Nathan-47@users.noreply.github.com>
2023-12-12 20:17:06 +0000

    Add files via upload

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,51 @@
+import {stagger, useAnimate, usePresence } from "framer-motion";
+import { useEffect } from 'react';
 
 
 const Contact = () => {
+
+    
+ // animate on view hook
+ const [isPresent, safeToRemove] = usePresence()
+ const [scope, animate] = useAnimate()
+ 
+ useEffect (() => {
+     if(isPresent) {
+       const enterAnimation = async () => {
+       await animate(
+         scope.current,
+         { opacity: [0, 7]},
+         { duration: 0.9, delay: stagger(0.3, {ease: "easeInOut"})},
+       )
+     }
+     enterAnimation()
+   
+   } else {
+     const exitAnimation = async () => {
+       await animate(scope.current, { opacity: 0 })
+       safeToRemove()
+   }
+   exitAnimation()
+ }
+ })
+   // animate on view hook
+
+
     return (
-        <p>Hello Contact</p>
-    )
+    <div ref={scope}
+    className='width-body'>
+        <div className='about-heading'>
+            <p>| Contact</p>
+            </div>
+            <p id="contact-content">If you are here then that means I’m fit for the job right?... 
+            <br /><br />
+            
+            Well unfortunately I’m not looking for anything at this moment in time but you can still drop me a message.
+
+            <p className="hr-lines"><a href="mailto:nathan.parchment47@gmail.com&subject=Job Offer">Drop a message</a></p>
+            </p>
+        </div>
+    );
 }
 
 export default Contact;
