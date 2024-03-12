a70def4892451abf32107623712f0cd0d525061f
Nathan Parchment <54561224+Nathan-47@users.noreply.github.com>
2024-03-12 22:28:13 +0000

    Add files via upload

--- src/pages/about.js
+++ src/pages/about.js
@@ -40,36 +40,40 @@ useEffect (() => {
             </div>
             <div class="grid grid-flow-col grid-rows-1 gap--40 flex justify-evenly">
   <div class='rotate-0'>
-    <img className="imagemiddle" src={image1} alt="nathan standing looking away from camera" width={200} loading="lazy" />
+    <img className="imagemiddle" src={image1} alt="image of nathan" width={200} loading="lazy" />
   </div>
       </div>
 
       <p className="heading-about">Nathan Parchment</p>
       <p className="heading-about-two">I code and design.</p>
 
-      <p id="text-about">Hi <span>&#9995;&#127998;</span> I’m Nathan. 
-      <br /><br />
-      I’m a web developer currently working at <a className='a-link' href='https://www.bcusu.com/' target='blank'>BCUSU</a> , An amazing independent charity, that gives students’ their best experience at University. 
-      <br /><br />
-
-      The main focus is to build and design accessible, unique and smooth student digital experiences, to generate more eyes on our website. I also take care of the back end such as overseeing data, handling logging issues and using GA4 analytics to show site performances plus more. 
+      <p id="text-about">Hi <span>&#9995;&#127998;</span> 
+        I’m a web developer currently working at BCUSU, An amazing independent charity, that gives students’ their best experience at University.
       <br /><br />
 
-      When the coding stops, I’m either powerlifting, playing football  or chilled out playing video games.
+      The main focus is to build and design accessible, unique and smooth student digital experiences, which I enjoy!
       <br /><br />
+
+      Away from the desk, I am reading various books, and playing tactical games like Football Manager / God of War and going gym to Powerlift.
+      <br /><br />
+
+      <p><a className='tool-link1' href='https://github.com/Nathan-47'>Github<svg className='embed' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg></a>  <span></span> 
       
-      Latest tech I’ve used recently;
+      <a className='tool-link2' href='https://www.linkedin.com/in/nathanparchment/'>LinkedIn<svg className='embed' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg></a>
+      </p>
       </p>
       <br />
+      
+      
+      {/* bento grid boxes */}
+        <div className="grid grid-cols-1 gap-1 my-1">
+          <div className="tools-box">
+            <p className='tools-heading'>Latest tech I've used</p>
 
-      <div className="bg-white py-6 sm:py-10">
-      <div className="mx-auto grid max-w-3xl gap-x-8 gap-y-10 px-6 lg:px-8 xl:grid-cols-3">
-        {/* <div className="max-w-2xl">
-        </div> */}
-        <ul id="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-3 sm:gap-y-16 xl:col-span-3">
+            <ul id="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-1 sm:gap-y-6 xl:col-span-1">
             <li>
               <div className="flex items-center gap-x-3">
-                <img className="h-8 w-8 rounded-full" src={arrow} alt="" />
+                <img className="h-6 w-6 rounded-full" src={arrow} alt="arrow-right" />
                 <div>
                   <p className="text-sm font-semibold leading-6 text-gray-50">React.js</p>
                 </div>
@@ -79,7 +83,7 @@ useEffect (() => {
             </li>
             <li>
               <div className="flex items-center gap-x-3">
-                <img className="h-8 w-8 rounded-full" src={arrow} alt="" />
+                <img className="h-6 w-6 rounded-full" src={arrow} alt="arrow-right" />
                 <div>
                   <p className="text-sm font-semibold leading-6 text-gray-50">JavaScript</p>
                 </div>
@@ -87,7 +91,7 @@ useEffect (() => {
             </li>
             <li>
               <div className="flex items-center gap-x-3">
-                <img className="h-8 h-8 rounded-full" src={arrow} alt="" />
+                <img className="h-6 h-6 rounded-full" src={arrow} alt="arrow-right" />
                 <div>
                   <p className="text-sm font-semibold leading-6 text-gray-50">Wordpress</p>
                 </div>
@@ -98,7 +102,7 @@ useEffect (() => {
             {/* start tech row 2 */}
             <li>
               <div className="flex items-center gap-x-3">
-                <img className="h-8 h-8 rounded-full" src={arrow} alt="" />
+                <img className="h-6 h-6 rounded-full" src={arrow} alt="arrow-right" />
                 <div>
                   <p className="text-sm font-semibold leading-6 text-gray-50">Shopify</p>
                 </div>
@@ -106,7 +110,7 @@ useEffect (() => {
             </li>
             <li>
               <div className="flex items-center gap-x-3">
-                <img className="h-8 h-8 rounded-full" src={arrow} alt="" />
+                <img className="h-6 h-6 rounded-full" src={arrow} alt="arrow-right" />
                 <div>
                   <p className="text-sm font-semibold leading-6 text-gray-50">MSL(CMS)</p>
                 </div>
@@ -114,7 +118,7 @@ useEffect (() => {
             </li>
             <li>
               <div className="flex items-center gap-x-3">
-                <img className="h-8 h-8 rounded-full" src={arrow} alt="" />
+                <img className="h-6 h-6 rounded-full" src={arrow} alt="arrow-right" />
                 <div>
                   <p className="text-sm font-semibold leading-6 text-gray-50">Github</p>
                 </div>
@@ -123,8 +127,8 @@ useEffect (() => {
              {/* end tech row 2 */}
 
         </ul>
-      </div>
-    </div>
+              </div>
+              </div>
       </div>
     )
 }
